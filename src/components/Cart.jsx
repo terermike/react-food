0b454c5c5d9e7a4f09bd2c8e3edf71c56088a029
Terer <terermike3@gmail.com>
2024-06-09 00:00:22 +0300

    Added Cart component for displaying cart items

---
+++ src/components/Cart.jsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import CartContext from "../store/CartContext.jsx";
+import Modal from "./UI/Modal.jsx";
+import currencyFormatter from "../utils/currencyFormatter.js";
+import Button from "./UI/Button.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+  const cartTotal = cartCtx.items.reduce((total, item) => {
+    return total + item.quantity * item.price;
+  }, 0);
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly>Close</Button>
+        <Button>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
