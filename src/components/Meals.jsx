c950411da389b1ac6d2e0a7140e6c34beac02ae9
Terer <terermike3@gmail.com>
2024-06-12 19:57:02 +0300

    Modified the files to enable clearing of cart after order placement

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,22 +1,23 @@
 import { useState, useEffect } from "react";
 import MealItem from "./MealItem";
-import CartContext from "../store/CartContext.jsx";
+import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
+
+const configData = {};
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", configData, []);
 
-      if (!response.ok) {
-        //
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-      console.log(loadedMeals);
-    }
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>;
+  }
 
-    fetchMeals();
-  }, []);
+  if (error) {
+    return <Error title="failed to fetch meals" message={error} />;
+  }
 
   return (
     <ul id="meals">
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -42,6 +43,10 @@ function cartReducer(state, action) {
     }
     return { ...state, items: updatedItems };
   }
+
+  if (action.type === "CLEAR_CART") {
+    return { ...state, items: [] };
+  }
   return state;
 }
 
@@ -55,10 +60,15 @@ export function CartContextProvider({ children }) {
   function removeItem(id) {
     dispatchCartAction({ type: "REMOVE_ITEM", id });
   }
+
+  function clearCart() {
+    dispatchCartAction({ type: "CLEAR_CART" });
+  }
   const cartContext = {
     items: cart.items,
     addItem,
     removeItem,
+    clearCart,
   };
   console.log(cartContext);
   return (
